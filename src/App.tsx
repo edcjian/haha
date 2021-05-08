679285ec67c674aac255447da367f8d18e36afab
edcjian <2015563489@qq.com>
2021-05-08 08:22:57 +0800

    默认的

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {gql} from "@apollo/client/core";
+import {ApolloClient, ApolloProvider, InMemoryCache, useQuery} from '@apollo/client';
+import {Home} from "./components/Home";
+import {Lists} from "./components/List";
+import {Route} from "react-router-dom";
+import {MyRoute} from "./components/Routers";
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:8080/graphql"
+});
 function App() {
   return (
+      <ApolloProvider client={client}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Home/>
+{/*      <Kot ke={12}/>*/}
+
     </div>
+      </ApolloProvider>
   );
 }
+
 
 export default App;
